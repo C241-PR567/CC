34910e9540bf9326d817a3f51109a3f7e9a54dea
zhanaaql <zulfahanaaqliyah@gmail.com>
2024-06-18 07:22:14 +0700

    change config

--- db-config/firebase-config.js
+++ db-config/firebase-config.js
@@ -3,16 +3,18 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { firebaseApp, admin } from '../src/firebaseAdmin.js';
+import dotenv from "dotenv";
 
+dotenv.config();
 const firebaseConfig = {
-  apiKey: "AIzaSyApj_lBE3j7hw-iEb_iA41nzXyFKxibelQ",
-  authDomain: "capstone-project-c241-pr567.firebaseapp.com",
-  databaseURL: "https://capstone-project-c241-pr567.asia-southeast1.firebasedatabase.app/",
-  projectId: "capstone-project-c241-pr567",
-  storageBucket: "skin-predict",
-  messagingSenderId: "928838313614",
-  appId: "1:928838313614:web:9ff46d7181b569b52b4bdf",
-  measurementId: "G-4837171KW4"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket:  process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
